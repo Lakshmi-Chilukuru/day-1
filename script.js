f9c98cc8fc980e3eb3b0748ae146fd62081c905d
Lakshmi-Chilukuru <laskhminarayana.chilukuru@gmail.com>
2024-01-13 23:41:43 +0530

    drums play-2

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
 //     key.classList.add('playing');
 // });
 function playSound(e){
+    
     const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key=document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) return;
